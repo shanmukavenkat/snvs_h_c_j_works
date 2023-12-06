71199ed3a4582a41601c34e4a789a9f8553586fc
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-06 22:45:00 +0530

    just sample used

---
+++ new_sample.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log(4-2,"subtracted by 2");
+console.log(4+2,"addition by 2");
+console.log(4*2,"multiplied by 2");
+console.log(4/2,"divided by 2");
+console.log(4%2,"modulus by 2");
