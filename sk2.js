15356d7bbd0feacb5500b4ef8bf38a3cdb8e5d99
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-08 19:10:00 +0530

    just name

---
+++ sk2.js
@@ -0,0 +1,9 @@
+
+function manipulateStyles(){
+    document.getElementById("HeadingElement").textContent = "S.N.V.S.KOMAL"
+    document.getElementById("HeadingElement").style.color = "blue"
+}
+function manipulateStyles1(){
+    document.getElementById("HeadingElement").textContent = "SHANMUKHA NAGA VENKATA SATYA KOMAL"
+    document.getElementById("HeadingElement").style.color = "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)"
+}
---
+++ snvs2.js
@@ -0,0 +1,5 @@
+;
+function manipulatesStyles(){
+    document.getElementById("headingElement").textContent="4.0 technologies";
+    document.getElementById("headingElement").style.color="violet";
+}
