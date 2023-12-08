fb12c5f9b39df145fe5a8aa0ac522ca3ef0f0582
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-08 21:47:51 +0530

    JAVASCRIPT_WORKS

---
+++ new directory/PRIMITIVE TYPES.js
@@ -0,0 +1,22 @@
+let a = 7831;
+let b = 7.576810;
+console.log(typeof(a));
+console.log(typeof(b));
+<!-- here no float and no integer here it prints the output as the number  -->
+<!-- here the number is the primitive type -->
+let boll = false;
+console.log(typeof(boll));
+console.log(typeof(true));
+<!-- here the boolean is the primitive type -->
+let name= "snvs";
+let name1 = 'snvs';
+let name2 = `snvs`;
+console.log(typeof(name));
+console.log(typeof(name1));
+console.log(typeof(name2));
+<!-- here the string is the primitive type -->
+let age;
+console.log(age);
+console.log(typeof(age));
+<!-- here the undefined is the primitive type -->
+<!--here the value undefined and type is undefined-- by default it take the undefined as a value when not assigned any variable -->
