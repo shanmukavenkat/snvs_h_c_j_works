6a338fcee861f74127b864e3c36d223aac96d39e
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-08 06:08:09 +0530

    cat

---
+++ cat.js
@@ -0,0 +1,17 @@
+function switchOff() {
+    document.getElementById("bulbimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent="Switched Off";
+    document.getElementById("offSwitch").style.backgroundColor = "red";
+    document.getElementById("onSwitch").style.backgroundColor = "white";
+}
+
+function switchOn() {
+    document.getElementById("bulbimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent="Switched On";
+    document.getElementById("offSwitch").style.backgroundColor = "white";
+    document.getElementById("onSwitch").style.backgroundColor = "green";
+
+
+}
