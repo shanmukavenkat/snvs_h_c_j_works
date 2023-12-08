5fbf93e0ec070102b1d883b8090c51843d166867
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-08 21:33:04 +0530

    JAVASCRIPT_WORKS

---
+++ new directory/snvskomal.js
@@ -0,0 +1,7 @@
+function newChange(){
+    document.getElementById("newText").style.color = "#cc9cff";
+    document.getElementById("newText").style.fontSize = "50px";
+    document.getElementById("newText").style.fontFamily = "Comic Sans MS";
+    document.getElementById("newText").style.backgroundColor = "#ffccff";
+    document.getElementById("newText").style.border = "2px solid #ffccff";
+}
