5b41e6ab716df3e2e63c6f62904ebb813295b352
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-16 23:20:20 +0530

    ARRAY IN JS

---
+++ new directory/array_IN_JS.js
@@ -0,0 +1,12 @@
+let myArray = [5,343.9,"snvskomal",4684];
+console.log(myArray);
+//we can access the elements of an array using index
+//here indexing starts from 0
+console.log(myArray[0]);
+console.log(myArray[3]);
+//here we can modify the elements of the array
+//here the array is mutable which means we can change the values of the array
+myArray[0]=10;
+console.log(myArray);
+// we can check the length the array using length property
+console.log(myArray.length);
