0d246fe4219ae449124fc7785a06da7367c2699e
SNVSKOMAL <21bq1a4208@vvit.net>
2023-12-18 23:08:17 +0530

    something in css

---
+++ new directory/array_Methods_in_js.js
@@ -0,0 +1,12 @@
+let myArray = [5,343.9,"snvskomal",4684];
+console.log(myArray);
+//here we can perform the various types of methods on the array
+//1)push method
+//this method adds the element in the end of the array
+myArray.push("hello");
+myArray.push(2342342);
+console.log(myArray);
+//2)pop method
+//this method removes the last element of the array
+myArray.pop();
+console.log(myArray);
