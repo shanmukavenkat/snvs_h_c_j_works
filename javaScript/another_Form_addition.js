0e7c7ade4c9f00e6468aaf89703f1eb13bc78fb1
SNVSKOMAL <21bq1a4208@vvit.net>
2024-01-11 23:32:33 +0530

    snvs_java_script_using_array_find_out_by_namw

--- javaScript/another_Form_addition.js
+++ javaScript/another_Form_addition.js
@@ -3,3 +3,9 @@ let numbers = [1, 2, 3, 4, 5];
 // Using reduce to calculate the sum of all numbers in the array
 
 console.log(sum); // Output: 15
+
+
+for (let invention of arrayOfInventions) {
+    let eachObjectKeyValue = invention[objectKey];
+    console.log(eachObjectKeyValue);
+}
