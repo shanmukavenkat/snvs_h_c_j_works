f17c3ecdd4e75822ecec15ef6751c8d00041888f
SNVSKOMAL <21bq1a4208@vvit.net>
2024-01-11 23:36:05 +0530

    snvs_java_script_addition

--- javaScript/another_Form_addition.js
+++ javaScript/another_Form_addition.js
@@ -3,6 +3,7 @@ let numbers = [1, 2, 3, 4, 5];
 // Using reduce to calculate the sum of all numbers in the array
 
 console.log(sum); // Output: 15
+console.log([sum]);
 
 
 for (let invention of arrayOfInventions) {
