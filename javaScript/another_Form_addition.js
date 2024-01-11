103820a5a8b0cd7be58ab0e2597e0adfaacf7588
SNVSKOMAL <21bq1a4208@vvit.net>
2024-01-11 22:23:25 +0530

    snvs_java_script

--- javaScript/another_Form_addition.js
+++ javaScript/another_Form_addition.js
@@ -1,6 +1,5 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Using reduce to calculate the sum of all numbers in the array
-let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
 console.log(sum); // Output: 15
