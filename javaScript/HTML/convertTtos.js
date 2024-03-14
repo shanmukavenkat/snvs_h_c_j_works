9d24a499bd823b831698b3e9b8ebed26ba2721ce
SNVSKOMAL <21bq1a4208@vvit.net>
2024-03-14 23:07:37 +0530

    min_and_hrs__>to<seconds

---
+++ javaScript/HTML/convertTtos.js
@@ -0,0 +1,19 @@
+let hoursInput = document.getElementById("hoursInput");
+let minuInput = document.getElementById("minutesInput");
+let declareMsg = document.getElementById("errorMsg");
+let dispMsg = document.getElementById("timeInSeconds");
+let buttonElement = document.getElementById("convertBtn");
+
+buttonElement.addEventListener("click",function(){
+
+    if (hoursInput.value === "" ){
+        declareMsg.textContent = "Please Enter Valid Number of Hours";
+
+    } else if (minuInput.value === ""){
+        declareMsg.textContent = "Please Enter Valid Number of Minutes";
+
+    } else {
+        let number = parseInt(hoursInput.value)*3600 + parseInt(minuInput.value)*60;
+        dispMsg.textContent = number +"s";
+    }
+});
