3ff9e7ff488bf507487c34d4ae00c8d0eca9099d
SNVSKOMAL <21bq1a4208@vvit.net>
2024-03-22 22:00:31 +0530

    fetch-operation-using-js

---
+++ javaScript/fetch1/get.js
@@ -0,0 +1,28 @@
+let sendGetRequestBtnEl = document.getElementById("sendGetRequestBtn");
+let loadingEl = document.getElementById("loading");
+let requestStatusEl = document.getElementById("requestStatus");
+let httpResponseEl = document.getElementById("httpResponse");
+
+function sendGetHTTPRequest() {
+    let url = "https://gorest.co.in/public-api/users";
+    let options = {
+        method: "GET",
+    };
+
+    loadingEl.classList.remove("d-none");
+    requestStatusEl.classList.add("d-none");
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            requestStatusEl.classList.remove("d-none");
+            loadingEl.classList.add("d-none");
+
+            let requestStatus = data.code;
+            let httpResponse = JSON.stringify(data);
+            requestStatusEl.textContent = requestStatus;
+            httpResponseEl.textContent = httpResponse;
+        });
+}
+sendGetRequestBtnEl.addEventListener("click", sendGetHTTPRequest);
