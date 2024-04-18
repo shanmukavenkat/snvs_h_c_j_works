f6fe806a18ed99febafd47a823b5ef7d3f36f36c
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-04-18 23:31:46 +0530

    random

--- javaScript/HTML/Random.js
+++ javaScript/HTML/Random.js
@@ -2,8 +2,11 @@ let buttonEle = document.getElementById("getActivityBtn");
 let imgSrc = document.getElementById("activityImg");
 let textOne = document.getElementById("activityName");
 let textTwo = document.getElementById("activityType");
+let spinnerEle = document.getElementById("spinner");
 
 function displayResult(data) {
+    spinnerEle.classList.add("d-none"); // Show spinner when making the API call
+
     textOne.textContent = data.activity;
     // You can assign any image source you want for the activity
     imgSrc.src = data.image; // Update with actual image source
@@ -11,6 +14,8 @@ function displayResult(data) {
 }
 
 buttonEle.addEventListener("click", function() {
+    spinnerEle.classList.remove("d-none"); // Show spinner when making the API call
+
     let options = {
         method: "GET"
     };
