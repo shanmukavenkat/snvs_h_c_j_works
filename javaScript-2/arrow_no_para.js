cd9965d1db86a83a4c58cf9e5594f3a5cf2d6822
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-03 22:10:55 +0530

    javscript_2

--- javaScript-2/arrow_no_para.js
+++ javaScript-2/arrow_no_para.js
@@ -1,2 +1,14 @@
 let sayHi = () => "Hello";
 console.log(sayHi());
+//returning object
+console.log("Returning object");
+let createPerson = name =>{
+
+    return {
+        name: name
+    }
+
+};
+console.log(createPerson("John"));
+let person = nameE => ({name: nameE});
+console.log(person("John"));
