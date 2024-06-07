5e9eddb4ad1b37a26f311cc17d72398995acee65
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-07 21:21:00 +0530

    javscript_2

---
+++ javaScript-2/prototype_2.js
@@ -0,0 +1,16 @@
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+//these instance properties are created for each object
+Person.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
+let person1 = new Person("John", "Doe");
+let person2 = new Person("Virat", "Kohli");
+//console.log(Object.getPrototypeOf(person1));
+//console.log(person2);
+console.log(person1.fullName());
+console.log(Object.getPrototypeOf(person1) === Object.getPrototypeOf(person2));
+
+console.log(person2.fullName());
