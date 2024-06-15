16f3f7f806f279a13b8e3929bf48eeb45905e3a9
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-15 21:29:14 +0530

    ROUGH_WRK

--- snvs2.js
+++ snvs2.js
@@ -1,4 +1,4 @@
-;
+console.log(document.getElementById("headingElement"));
 function manipulatesStyles(){
     document.getElementById("headingElement").textContent="4.0 technologies";
     document.getElementById("headingElement").style.color="violet";
