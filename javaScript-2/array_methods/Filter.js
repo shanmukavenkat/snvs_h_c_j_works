09c1a2fccecf27d2e7083be46fe3f6e4749aa232
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-17 10:09:58 +0530

    immutable-array-methods

---
+++ javaScript-2/array_methods/Filter.js
@@ -0,0 +1,3 @@
+const numbers = [1,2,-3,-7,-456,234,345];
+const positiveNumbers = numbers.filter((number) => number > 0);
+console.log(positiveNumbers);
---
+++ javaScript-2/array_methods/map.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map((number) => number * number);
+console.log(squaredNumbers); 
