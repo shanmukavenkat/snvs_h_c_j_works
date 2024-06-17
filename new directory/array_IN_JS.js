c4d3d45c73fae28066e64df0190c8751945ca0dc
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-17 12:31:30 +0530

    button_change

--- new directory/array_IN_JS.js
+++ new directory/array_IN_JS.js
@@ -1,9 +1,10 @@
-let myArray = [5,343.9,"snvskomal",4684];
+let myArray = [5,0,2,343.9,"snvskomal",4684];
 console.log(myArray);
+console.log(myArray[1]*myArray[2]);
 //we can access the elements of an array using index
 //here indexing starts from 0
 console.log(myArray[0]);
-console.log(myArray[3]);
+console.log(myArray[4]);
 //here we can modify the elements of the array
 //here the array is mutable which means we can change the values of the array
 myArray[0]=10;
