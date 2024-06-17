68690399e2d573ef56341f0a0623b2068f243e15
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-17 09:55:53 +0530

    Hoisting

---
+++ javaScript-2/Hoisting/Hoisting.js
@@ -0,0 +1,10 @@
+let x = 20;
+let y = 39;
+let result  = add(x,y);
+console.log(result);
+function add(a,b){
+    return a+b;
+}
+//hoisting is a javascript
+//mechanism where functions declarations
+//are moved to the top of there scope before code execution
---
+++ javaScript-2/Hoisting/hoisting-arrow-functions.js
@@ -0,0 +1,16 @@
+foo();
+let foo = () => {
+    let x = 20;
+    console.log(x);
+};
+///reference error
+//arrow functions are not hoisted
+
+
+GOO();
+let goo = function(){
+    let x = 20;
+    console.log(x);
+}
+///reference error
+//arrow functions are not hoisted
