9225f02955bcebc438d0032938966d688a4dc460
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-17 09:44:49 +0530

    scoping

--- javaScript-2/Scoping/GlobalScopr.js
+++ javaScript-2/Scoping/GlobalScopr.js
@@ -1,5 +1,5 @@
 //if a variable is declared
-//out side all the functions and curly braces
+//outside all the functions and curly braces
 //it is called as global scope
 let x = 30;
 function Foo(){
