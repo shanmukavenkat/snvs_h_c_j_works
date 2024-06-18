e9e95f4434e1234b53a44a3c21b0b356ff6f22c8
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-18 23:15:59 +0530

    string-manipulations

---
+++ javaScript-2/string_manipulations/convert-array-to-normalstring.js
@@ -0,0 +1,6 @@
+const nestedArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+// Flatten the nested array and join elements with spaces
+const flattenedArray = nestedArray.flat().join(" ");
+
+console.log(flattenedArray); // Output will be "1 2 3 4 5 6 7 8 9"
