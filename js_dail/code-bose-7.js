216e02e21bcb7a440b1780e70e38e4a360cb639b
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-23 22:16:41 +0530

    the-code-bose

--- js_dail/code-bose-7.js
+++ js_dail/code-bose-7.js
@@ -8,3 +8,10 @@ user2.followers = user1.followers ;
 
 console.log(user1.followers);
 console.log(user2.followers);
+
+
+let y = 0;
+//increments to  1 but
+//returns old value ,i.e., 0
+y = y++;
+console.log(y);
