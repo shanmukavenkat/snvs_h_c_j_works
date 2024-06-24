078b504272a73c8fe2367765002de8c01c455d0d
S.N.V.S.KOMAL <113082454+shanmukavenkat@users.noreply.github.com>
2024-06-24 20:24:16 +0530

    the-code-bose

---
+++ js_dail/code-bose-8.js
@@ -0,0 +1,7 @@
+let x = new Number("1");
+console.log(x);
+let y = Number("1");
+console.log(y);
+
+console.log(typeof(x));
+console.log(typeof(y));
---
+++ js_dail/code-bose-9.js
@@ -0,0 +1,5 @@
+const greeting = "Hello";
+//string is immutable
+greeting.length = 10;
+console.log(greeting.length);
+console.log(greeting);
