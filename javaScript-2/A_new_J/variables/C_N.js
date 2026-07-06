9de871160c2cc84e034eb3a88a255bd932db5d94
shanmukavenkat <21bq1a4208@vvit.net>
2026-07-06 15:24:53 +0530

    OLD Stuffs

---
+++ javaScript-2/A_new_J/variables/C_N.js
@@ -0,0 +1,29 @@
+const uid = "abcdefghijkl";
+
+
+var fullName = "snvskomal"
+var email = "shanmukavenkatkomal@gamil.com"
+var password = "12345678"
+var conformPassword = "12345678"
+var courseCount = 0;
+var isloggedInFrom_Google = false;
+//--> to take the user name form the user we use the "PROMPT" which must be connected to the html file fullName = prompt("enter your name");
+console.log(uid)
+console.log("full name is : " + fullName);
+console.log("user email: ",email);
+console.log(password);
+console.log(conformPassword);
+console.log(courseCount);
+
+console.log(`
+cousrsename:${uid}
+email:${email}
+password:${password}
+conformPassword:${conformPassword}
+courseCount:${courseCount}
+isloggedInFrom_Google:${isloggedInFrom_Google}
+email:${email}
+password:${password}
+
+
+`)
