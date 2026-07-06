949df1e465ca469cbf1fd4068b24d0999b857a93
shanmukavenkat <21bq1a4208@vvit.net>
2026-07-06 15:24:53 +0530

    OLD Stuffs

---
+++ javaScript-2/A_new_J/variables/L_V_C_2.js
@@ -0,0 +1,21 @@
+var fullName ="snvskomal";
+// this is naming the variables  like many ways
+// full_name,fullName
+// starting with no capital letters and using the
+// dashes and use snake case letters and make it readable and
+console.log("(--/comments..../--) comments in the javascript and the " +
+    "(/*multiline Comments .....*/) multiple line comments." );
+
+
+var courseName = "React na"
+var isLoggedin = false;
+// the smaller false and the smaller  true is defined
+// as the boolean keywords in the javascript
+var isLoggedCount = 34;
+
+var use_it_another_time;
+use_it_another_time = "using now"
+
+console.log(isLoggedin);
+console.log(isLoggedCount);
+console.log(use_it_another_time);
